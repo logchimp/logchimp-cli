8f0be0fe3e30a6b6bbeed14180e9fe3ac32c7999
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-09 13:41:24 +0530

    Add help command (usage, core, additional, flags, learn more and feedback) section

--- src/commands/help.js
+++ src/commands/help.js
@@ -1,10 +1,27 @@
+const chalk = require("chalk");
+
 class Help {
 	run() {
-		console.log(`logchimp [command]
+		console.log(`
+${chalk.bold("USAGE")}
+  logchimp <command> <subcommands> [flags]
 
-Commands:
-  logchimp install      Install a brand new instance of LogChimp
-  logchimp version      Prints out LogChimp-CLI version
+${chalk.bold("CORE COMMANDS")}
+  install      Install a brand new instance of LogChimp
+
+${chalk.bold("ADDITIONAL COMMANDS")}
+  help      Help about any command
+
+${chalk.bold("FLAGS")}
+  --help         Show help for command
+  --version      Show LogChimp-CLI version
+
+${chalk.bold("LEARN MORE")}
+  Use 'logchimp <command> <subcommand> --help' for more information about a command.
+  Read the manual at https://logchimp.codecarrot.net/docs/install/logchimp-cli/
+
+${chalk.bold("FEEDBACK")}
+  Open an issue at https://github.com/logchimp/logchimp-cli/issues
 `)
 	}
 };
