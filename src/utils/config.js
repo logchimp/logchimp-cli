de96dd2e4fe7bc2b79caaf22ce9cf0114ff49c13
Yashu Mittal <mittalyashu77@gmail.com>
2020-10-10 07:35:47 +0530

    Create Config class
    Use Config class to handle data inside "logchimp.config.json" file.

---
+++ src/utils/config.js
@@ -0,0 +1,62 @@
+const fs = require('fs-extra')
+
+class Config {
+  /**
+	 * Constructs the config instance
+	 *
+	 * @param {string} filename Filename to load
+	 */
+  constructor(filename) {
+    if (!filename) {
+      throw new Error('Config file not specified!')
+    }
+
+    this.file = filename
+    this.values = Config.exists(this.file) || {}
+  }
+
+  get(key) {
+    return this.values[key]
+  }
+
+  /**
+	 * Sets a value in the config.
+	 * If 'value' is null, removes the key from the config
+	 *
+	 * @param {Object} value Object to save in config file
+	 * @returns {Class} Config instance
+	 */
+  set(value) {
+    if (!value) {
+      throw new Error('value not found')
+    }
+
+    Object.assign(this.values, value)
+    return this
+  }
+
+  /**
+	 * Saves the config file to disk
+	 * @returns {Boolean} Return boolean after saving
+	 */
+  save() {
+    fs.writeJSONSync(this.file, this.values, {spaces: 2})
+    return true
+  }
+
+  /**
+	 * Checks whether or not a config file exists
+	 * @param {string} filename absolute path to config file
+	 * @returns {object} return parsed json format data from config file
+	 */
+  static exists(filename) {
+    try {
+      const result = fs.readJsonSync(filename)
+      return result
+    } catch (error) {
+      return false
+    }
+  }
+}
+
+module.exports = Config
