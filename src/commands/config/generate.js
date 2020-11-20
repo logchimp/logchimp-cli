c94768607cbd649aa497f1ce9afff342a6378ea3
Yashu Mittal <mittalyashu77@gmail.com>
2020-11-20 18:16:31 +0530

    Split logchimp setup questions into util

--- src/commands/config/generate.js
+++ src/commands/config/generate.js
@@ -1,11 +1,11 @@
 const {Command, flags} = require('@oclif/command')
 const path = require('path')
-const inquirer = require('inquirer')
 const omgopass = require('omgopass')
 const _ = require('lodash')
 
 // utils
 const Config = require('../../utils/config')
+const askQuestions = require('../../utils/ask-questions')
 
 class ConfigGenerateCommand extends Command {
   async run() {
@@ -26,123 +26,16 @@ class ConfigGenerateCommand extends Command {
       return
     }
 
-    // generate random password
-    const generateDatabasePassword = omgopass()
-    const generateSecretKey = omgopass({
-      minSyllableLength: 10,
-    })
-
-    const configDefaults = {
-      database: {
-        host: 'localhost',
-        user: 'logchimp',
-        password: generateDatabasePassword,
-        name: 'logchimp',
-        port: 5432,
-        ssl: true,
-      },
-      server: {
-        port: 3000,
-        secretkey: generateSecretKey,
-      },
-    }
-
-    // Check for --defaults flag
-    if (flags.defaults) {
-      config.set(configDefaults).save()
+    // Check for --interactive flag
+    if (flags.interactive) {
+      const generateConfig = await askQuestions()
+      config.set(generateConfig).save()
       this.log('LogChimp configuration file succesfully created.')
       return
     }
 
-    const generateConfig = await inquirer.prompt([
-      // server
-      {
-        type: 'input',
-        name: 'server.port',
-        message: 'Server port to listen on',
-        default: configDefaults.server.port,
-        validate: answer => {
-          // Check port is not empty and is number
-          const isNumber = !isNaN(answer)
-          if (answer !== '') if (isNumber) return true
-
-          return 'Please enter a correct port'
-        },
       },
-      {
-        type: 'input',
-        name: 'server.secretkey',
-        message: 'Secret key for password validation (default to auto generate random string)',
-        filter: answer => {
-          // Return auto generated secretKey on empty answer
-          if (answer === '') return generateSecretKey
-          return answer
-        },
       },
-      // database
-      {
-        type: 'input',
-        name: 'database.host',
-        message: 'Database host',
-        default: configDefaults.database.host,
-        validate: answer => {
-          const isString = isNaN(answer)
-
-          // Warn for special characters
-          const removeSpecialCharacters = answer.match(/[^.\w\s]/gi)
-          if (!_.isEmpty(removeSpecialCharacters)) return 'Special characters are not supported'
-
-          if (isString) return true
-          return 'Please enter a valid host'
-        },
-      },
-      {
-        type: 'input',
-        name: 'database.password',
-        message: 'Database password (default auto generate random password)',
-        filter: answer => {
-          // Return auto generated password on empty answer
-          if (answer === '') return generateDatabasePassword
-          return answer
-        },
-      },
-      {
-        type: 'input',
-        name: 'database.name',
-        message: 'Database name',
-        default: configDefaults.database.name,
-        validate: answer => {
-          const isString = isNaN(answer)
-
-          // Warn for special characters
-          const removeSpecialCharacters = answer.match(/[^\w\s]/gi)
-          if (!_.isEmpty(removeSpecialCharacters)) return 'Special characters are not supported'
-
-          if (isString) return true
-          return 'Please enter a valid name'
-        },
-      },
-      {
-        type: 'input',
-        name: 'database.port',
-        message: 'Database port',
-        default: configDefaults.database.port,
-        validate: answer => {
-          // Check port is not empty and is number
-          const isNumber = !isNaN(answer)
-          if (answer !== '') if (isNumber) return true
-
-          return 'Please enter a correct port'
-        },
-      },
-      {
-        type: 'confirm',
-        name: 'database.ssl',
-        message: 'Enable SSL for database (default true for production)',
-        choices: ['true', 'false'],
-        default: configDefaults.database.ssl,
-      },
-    ])
 
     config.set(generateConfig).save()
     this.log('LogChimp configuration file succesfully created.')
@@ -153,10 +46,9 @@ ConfigGenerateCommand.description = `Generate a new configuration for a LogChimp
 `
 
 ConfigGenerateCommand.flags = {
-  defaults: flags.boolean({
-    char: 'd',
-    description: 'Use defaults',
-    default: false,
+  interactive: flags.boolean({
+    char: 'i',
+    description: 'Use interactive mode',
   }),
   force: flags.boolean({
     char: 'f',
---
+++ src/utils/ask-questions.js
@@ -0,0 +1,173 @@
+const inquirer = require('inquirer')
+const omgopass = require('omgopass')
+const _ = require('lodash')
+
+const askQuestions = async () => {
+  // generate random password
+  const generatePassword = omgopass({
+    minSyllableLength: 12,
+  })
+
+  const generateConfig = await inquirer.prompt([
+    // server
+    {
+      type: 'confirm',
+      name: 'local',
+      message: 'Run LogChimp for local development/testing',
+      choices: ['false', 'true'],
+      default: false,
+    },
+    {
+      type: 'input',
+      name: 'server.port',
+      message: 'Server port to listen on',
+      default: 3000,
+      validate: answer => {
+        // Check port is not empty and is number
+        const isNumber = !isNaN(answer)
+        if (answer !== '') if (isNumber) return true
+
+        return 'Please enter a correct server port'
+      },
+    },
+    {
+      type: 'input',
+      name: 'server.secretkey',
+      message: 'Secret key for password validation (default to auto-generate random secret key)',
+      filter: answer => {
+        // Return auto generated secretKey on empty answer
+        if (answer === '') return generatePassword
+        return answer
+      },
+    },
+
+    // database
+    {
+      type: 'input',
+      name: 'database.name',
+      message: 'Database name',
+      validate: answer => {
+        if (_.isString(answer) && answer) {
+          return true
+        }
+        return 'Please enter a valid database name'
+      },
+    },
+    {
+      type: 'input',
+      name: 'database.host',
+      message: 'Database host',
+      validate: answer => {
+        const isString = isNaN(answer)
+
+        // Warn for special characters
+        const removeSpecialCharacters = answer.match(/[^.\w\s]/gi)
+        if (!_.isEmpty(removeSpecialCharacters)) return 'Special characters are not supported'
+
+        if (isString) return true
+      },
+    },
+    {
+      type: 'input',
+      name: 'database.port',
+      message: 'Database port',
+      default: 5432,
+      validate: answer => {
+        // Check port is not empty and is number
+        const isNumber = !isNaN(answer)
+        if (answer !== '') if (isNumber) return true
+
+        return 'Please enter a correct database port'
+      },
+    },
+    {
+      type: 'input',
+      name: 'database.user',
+      message: 'Database username',
+      validate: answer => {
+        if (_.isString(answer) && answer) {
+          return true
+        }
+        return 'Please enter a valid database username'
+      },
+    },
+    {
+      type: 'input',
+      name: 'database.password',
+      message: 'Database password (default auto-generate random password)',
+      filter: answer => {
+        // Return auto generated password on empty answer
+        if (answer === '') return generatePassword
+        return answer
+      },
+    },
+    {
+      type: 'confirm',
+      name: 'database.ssl',
+      message: 'Enable SSL for database',
+      choices: ['true', 'false'],
+    },
+
+    // mail
+    {
+      type: 'list',
+      name: 'mail.service',
+      message: 'Select a mail service of your choice',
+      choices: [
+        'mailgun',
+      ],
+    },
+    {
+      type: 'input',
+      name: 'mail.host',
+      message: 'Mail hostname',
+      validate: answer => {
+        const isString = isNaN(answer)
+
+        // Warn for special characters
+        const removeSpecialCharacters = answer.match(/[^.\w\s]/gi)
+        if (!_.isEmpty(removeSpecialCharacters)) return 'Special characters are not supported'
+
+        if (isString) return true
+      },
+    },
+    {
+      type: 'input',
+      name: 'mail.port',
+      message: 'Mail port',
+      default: 587,
+      validate: answer => {
+        // Check port is not empty and is number
+        const isNumber = !isNaN(answer)
+        if (answer !== '') if (isNumber) return true
+
+        return 'Please enter a correct mail port'
+      },
+    },
+    {
+      type: 'input',
+      name: 'mail.username',
+      message: 'Mail username',
+      validate: answer => {
+        if (_.isString(answer) && answer) {
+          return true
+        }
+        return 'Please enter a valid mail username'
+      },
+    },
+    {
+      type: 'input',
+      name: 'mail.password',
+      message: 'Mail password (default auto-generate random password)',
+      filter: answer => {
+        // Return auto generated password on empty answer
+        if (answer === '') return generatePassword
+        return answer
+      },
+    },
+  ])
+
+  return generateConfig
+}
+
+module.exports = askQuestions
