50fd0744b95bf68062e8510c466c947bfdfe4c03
Yashu Mittal <mittalyashu77@gmail.com>
2021-01-05 18:36:01 +0530

    write tests for config:get command

---
+++ tests/command/config-get.spec.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const execa = require('execa')
+const fs = require('fs-extra')
+
+const CLI_PATH = path.resolve(__dirname, '..', '..', 'bin', 'run')
+const runCommand = (args, options) => execa(CLI_PATH, args, options)
+
+describe('config:get command', () => {
+  it('config file missing error', async () => {
+    // fail-safe: delete any existing logchimp config at root directory
+    const currentDirectory = await process.cwd()
+    await fs.removeSync(`${currentDirectory}/logchimp.config.json`)
+
+    const command = await runCommand(['config:get'])
+    expect(command.stderr).toContain('Warning: Logchimp configuration file doesn\'t exist.')
+  })
+
+  it('--key flag is missing', async () => {
+    // generate config file
+    await runCommand(['config:generate'])
+    const command = await runCommand(['config:get'])
+
+    expect(command.stdout).toContain(
+      'Pass the --key flag to get the value',
+    )
+  })
+
+  it('get \'secretkey\' value', async () => {
+    const command = await runCommand(['config:get', '-k=database.port'])
+
+    expect(command.stdout).toBe(5432)
+  })
+})
